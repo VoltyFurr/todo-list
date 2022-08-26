09c9c4ff7fb44b023fde75ac2cfda4e67efee6b0
volty <voltyfurr@gmail.com>
2022-08-26 15:57:07 +0300

    Add initial styles

--- index.php
+++ index.php
@@ -5,9 +5,25 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>To-Do list</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
+<div class="main-section">
+    <div class="add-section">
+        <form action="">
+            <input type="text" name="title" placeholder="This field is required">
+            <button type="submit">Add &nbsp; <span>&#43;</span></button>
+        </form>
+    </div>
+    <div class="show-todo-section">
+        <div class="todo-item">
+            <input type="checkbox">
+            <h2>This is hhsx nknak</h2>
+            <br>
+            <small>created: 3/3/2020</small>
+        </div>
+    </div>
+</div>
 </body>
 </html>
