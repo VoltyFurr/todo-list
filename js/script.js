f11278c7a59833575bd4ed91c48752acf6ed78db
volty <voltyfurr@gmail.com>
2022-08-27 11:05:32 +0300

    Add delete notes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-$(document).ready(function (){
+$(document).ready(function () {
+    $('.remove-to-do').click(function () {
+        const id = $(this).attr('id');
 
+        $.post("app/remove.php",
+            {
+                id: id
+            },
+            (data) => {
+                if (data) {
+                    $(this).parent().hide(600);
+                }
+            }
+        );
+    });
 });
